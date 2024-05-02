2942cb42b2ef2afe2ed3d615c617c6467fa4a1d5
jonathanembleyriches <jonathanembleyriches@outlook.com>
2024-05-02 19:37:18 +0100

    update

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -32,10 +32,10 @@ return {
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
-lspconfig.pyright.setup({
+      lspconfig.pyright.setup({
         capabilities = capabilities
       })
-lspconfig.ruff_lsp.setup({
+      lspconfig.ruff_lsp.setup({
 
         capabilities = capabilities
       })
@@ -44,10 +44,10 @@ lspconfig.ruff_lsp.setup({
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
- vim.keymap.set("n", "<leader>gh", vim.lsp.buf.references, {})
- vim.keymap.set("n", "<leader>gj", function()
-    require("telescope.builtin").lsp_references()
-end, { noremap = true, silent = true })
+      vim.keymap.set("n", "<leader>gh", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>gj", function()
+        require("telescope.builtin").lsp_references()
+      end, { noremap = true, silent = true })
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
